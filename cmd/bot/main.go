cc8183ac47c1115fa6aea62441e2bd7288618fe5
Feyd-Rauth <atreides.pasha@yandex.ru>
2021-10-14 22:48:02 +0300

    Add simple hello world functionality

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,4 +1,47 @@
 package main
 
+import (
+	"fmt"
+	"github.com/Feyd-Rauth/Zlyden/pkg/zlyden"
+	"github.com/bwmarrin/discordgo"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 func main() {
+	// loading Discord bot token
+	token, err := loadToken("token.dat")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	discord.AddHandler(zlyden.MessageCreate)
+
+	if err := discord.Open(); err != nil {
+		log.Fatal(err)
+	}
+	defer discord.Close()
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+}
+
+func loadToken(filename string) (string, error) {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	return string(file), nil
 }
--- pkg/zlyden/zlyden.go
+++ pkg/zlyden/zlyden.go
@@ -1,1 +1,25 @@
 package zlyden
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"strings"
+)
+
+func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "Say Hi" reply with "Hello World!"
+	if strings.EqualFold(m.Content, "Say Hi") {
+		s.ChannelMessageSend(m.ChannelID, "Hello World!")
+	}
+
+	// If the message is "Say Goodbye" reply with "Goodbye World!"
+	if strings.EqualFold(m.Content, "Say Goodbye") {
+		s.ChannelMessageSend(m.ChannelID, "Goodbye World!")
+		s.Close()
+	}
+}
