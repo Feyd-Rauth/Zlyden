57839f61ab819f98d4a557c09fe6d037eaf08ef1
Feyd-Rauth <69901637+Feyd-Rauth@users.noreply.github.com>
2021-10-15 17:45:41 +0300

    Simple hello world functionality added (#3)
    * Add simple hello world functionality

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,4 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Feyd-Rauth/Zlyden/pkg/zlyden"
+	"github.com/bwmarrin/discordgo"
+)
+
 func main() {
+	// loading Discord bot token
+	token, err := loadToken("token.dat")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	discord.AddHandler(zlyden.MessageCreate)
+
+	if err := discord.Open(); err != nil {
+		log.Fatal(err)
+	}
+	defer func(discord *discordgo.Session) {
+		err := discord.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(discord)
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+}
+
+func loadToken(filename string) (string, error) {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	return string(file), nil
 }
--- pkg/zlyden/zlyden.go
+++ pkg/zlyden/zlyden.go
@@ -1,1 +1,36 @@
 package zlyden
+
+import (
+	"log"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "Say Hi" reply with "Hello World!"
+	if strings.EqualFold(m.Content, "Say Hi") {
+		_, err := s.ChannelMessageSend(m.ChannelID, "Hello World!")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// If the message is "Say Goodbye" reply with "Goodbye World!"
+	if strings.EqualFold(m.Content, "Say Goodbye") {
+		_, err := s.ChannelMessageSend(m.ChannelID, "Goodbye World!")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := s.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
